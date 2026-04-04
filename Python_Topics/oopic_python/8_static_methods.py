5ac99405c254fdd41bcb726a4f671e3d5059f066
Shriniwas Kulkarni <kshriniwas180205@gmail.com>
2026-04-04 13:03:43 +0530

    Update 8_static_methods.py

--- Python_Topics/oopic_python/8_static_methods.py
+++ Python_Topics/oopic_python/8_static_methods.py
@@ -1,5 +1,6 @@
 """
 static methods are just regular functions that
 cannot access self or class, these are just 
-tied to classes for easy access
+tied to classes for easy access, but stored in 
+the class __dict__.
 """
